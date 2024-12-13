c6e219f9821a76aa6145f578be5390e520a8bb51
MateenIqbal1 <mateeniqbal840@gmail.com>
2024-12-13 13:13:06 +0500

    Remove line-clamp plugin from tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-
 export default {
   content: [
     "./index.html",
@@ -9,6 +8,6 @@ export default {
     extend: {},
   },
   plugins: [
-    lineClamp,
+    // Remove lineClamp plugin
   ],
 };
