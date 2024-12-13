dfd9f6044f0a273d4c4e26aefbdb25fbda83578a
MateenIqbal1 <mateeniqbal840@gmail.com>
2024-12-13 17:24:51 +0500

    Removed api folder from repository

--- api/routes/imageRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {upload} from '../helpers/cloudinary.js'
-import { handleImageUpload } from '../controllers/imageController.js'
-
-const router = express.Router();
-
-router.post('/upload', upload.array('images', 6), handleImageUpload);
-
-export  {router};
